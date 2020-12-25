a3e6e59ddff38bd0ceb9686b5d5257b3bc39ca41
ash000es <ash007.uk@icloud.com>
2020-12-26 00:46:36 +0100

    chnaging schema

--- api/models/hotelProduct.js
+++ api/models/hotelProduct.js
@@ -1,17 +1,12 @@
 const mongoose = require('mongoose')
 
 const hotelproductSchema = mongoose.Schema({
-    
-           hotelName:{type:String, required: true},
-           location:{type:String, required: true},
-           productInventory: { type: Number, required: true },
-           images:[{type: String }],
-           amenities:[{type: String}],
-           rooms:[{type: Object }],
-           date: { type: Date, default: Date.now }
-          
-        
-        
+  hotelName: { type: String, required: true },
+  location: { type: String, required: true },
+  images: [{ type: String }],
+  amenities: [{ type: String }],
+  rooms: [{ type: Object }],
+  date: { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('HotelProduct', hotelproductSchema)
--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   )
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With', 'GET, PUT, POST, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'GET, PUT, POST, PATCH, DELETE')
     return res.status(200).json({})
   }
   next()
