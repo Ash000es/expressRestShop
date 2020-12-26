0197b34fccea31958d689736a0cb62404c4f82b8
ash000es <ash007.uk@icloud.com>
2020-12-26 21:44:02 +0100

    filling schema types

--- api/models/hotelbeds.js
+++ api/models/hotelbeds.js
@@ -1,10 +1,33 @@
 const mongoose = require('mongoose')
 
 const HotelBedsSchema = mongoose.Schema({
-  
-  name: {type: Object },
-  images:[{type: Object }],
-
+  createdBy: { type: mongoose.Types.ObjectId, required: false },
+  accommodationType: { type: String, required: true },
+  categoryCode: { type: String, required: false },
+  categoryName: { type: String, required: false },
+  contactDetails: { type: Object, required: true },
+  code: { type: Number, required: false },
+  name: { type: String, required: true },
+  city: { type: String, required: true },
+  postCode: { type: String, required: false },
+  areaName: { type: String, required: false },
+  address: { type: Object, required: true },
+  email: { type: String, required: false },
+  phones: { type: String, required: false },
+  destinationCode: { type: String, required: false },
+  destinationName: { type: String, required: false },
+  latitude: { type: String, required: false },
+  longitude: { type: String, required: false },
+  images: [{ type: Object }],
+  amenities: [{ type: String, required: true }],
+  reviews: [{ type: Object }],
+  rooms: [{ type: Object }],
+  minRate: { type: String, required: false },
+  maxRate: { type: String, required: false },
+  currency: { type: String, required: false },
+  license: { type: String, required: false },
+  label: { type: String, required: false },
+  Extras: [{ type: Object, required: false }],
   date: { type: Date, default: Date.now }
 })
 
