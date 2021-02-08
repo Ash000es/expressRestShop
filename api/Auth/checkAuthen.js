0f24d720b2ccb40fc370d977a945196c537f7ac4
ash000es <ash007.uk@icloud.com>
2021-02-08 17:40:50 +0100

    cleaning clg

--- api/Auth/checkAuthen.js
+++ api/Auth/checkAuthen.js
@@ -5,12 +5,10 @@ module.exports = (req, res, next) => {
   try {
     // const token = req.headers.authorization.split(' ')[1]
     const token = req.cookies.token
-    console.log(token, 'token in checkauth')
     const decoded = jwt.verify(token, process.env.JWT_KEY)
     req.userData = decoded
     next()
   } catch (err) {
-    console.log('failed here')
     return res.status(401).json({
       message: 'Auth failed'
     })
